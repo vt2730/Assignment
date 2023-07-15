2bf99ef2860b56094144104ad902eb5d689ffd0e
uplvikash <vikash.tiwari@underscoretec.com>
2023-07-15 12:38:25 +0530

    sidenave routing added.

--- frontend/src/container/Admin/LibTransaction/index.jsx
+++ frontend/src/container/Admin/LibTransaction/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import LibraryTransaction from '../../../components/Admin/LibTransaction'
+import SideBar from '../../../components/Layout/SideBar/SideBar'
+import TwohalfLayout from '../../../components/Layout/TwohalfLayout'
+import Headerlayout from '../../../common/Headerlayout'
 
 const LibraryTransactionMain = () => {
   return (
-    <div>LibraryTransactionMain</div>
+    <div>
+      <TwohalfLayout
+          sidebarsection={<SideBar />}
+          rendercomponent={<LibraryTransaction />}
+          isHeader={true}
+
+          commonheader={<Headerlayout
+            name={"Book Details"}
+            isbutton={false}
+            addbutton={false}
+          />}
+        />
+    </div>
   )
 }
 
