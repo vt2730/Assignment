15c25c7501d47a951846c7a6969ecb0477cddbe6
uplvikash <vikash.tiwari@underscoretec.com>
2023-07-16 05:18:23 +0530

    login and signup api integrated

--- frontend/src/container/Authentication/Login/Hooks.js
+++ frontend/src/container/Authentication/Login/Hooks.js
@@ -1,11 +1,12 @@
 import { useFormik } from 'formik';
 import * as Yup from "yup"
 import { doPostApiCall } from '../../../utils/ApiConfig';
-
+import { useNavigate ,useLocation} from "react-router"
 
 const PUBLIC_apiurl = 'http://192.168.1.6:5000/api';
 export const ForLogin = () => {
 
+    const navigate = useNavigate();
 
     const LoginForm = useFormik({
         initialValues: {
@@ -35,8 +36,10 @@ export const ForLogin = () => {
         }
         doPostApiCall(data)
             .then((res)=>{
-                if(!res.error){
-                    
+                if(res?.code === "LOGIN_SUCCESSFUL"){
+                    localStorage.setItem('role', res?.result?.role);
+                    localStorage.setItem('userId', res?.result?._id);
+                    navigate("/admin/bookdetails")
                 }
             })
             .catch((err)=>{
